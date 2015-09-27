2f11e9ad1838c4e92705e48305e7e1bab83f95fa
Bing Li <enst.bupt@gmail.com>
2015-09-27 09:49:39 -0400

    0.0.0.0

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 chown bigcouch:daemon /srv /var/log/bigcouch -R
 
 [ -n $COOKIE ] || COOKIE=`date +%s | sha256sum | base64 | head -c 32`
-sed -i '/bind_address/s/0.0.0.0/127.0.0.1/' /etc/kazoo/bigcouch/local.ini 
+#sed -i '/bind_address/s/0.0.0.0/127.0.0.1/' /etc/kazoo/bigcouch/local.ini 
 sed -i "/-setcookie/s/change_me/$COOKIE/" /etc/kazoo/bigcouch/vm.args
 
 service bigcouch start
